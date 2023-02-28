e9db8085988e258a86bfd28edbfe84c9c2edc744
messimk <mekuriyawmeseret09@gmail.com>
2023-02-28 15:37:37 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "kasiye.h"
+
+
+/**
+ * reset_to_98n - take a pointer and updates
+ * the value it points to to 98
+ * @n: point to an int
+ * return: noting
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}

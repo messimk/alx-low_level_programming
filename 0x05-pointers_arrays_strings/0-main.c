f90078e74ac8f142bf2034ee629c95d13474018a
messimk <mekuriyawmeseret09@gmail.com>
2023-03-04 05:45:43 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - ckeck the code
+ *
+ * return Always 0,
+ */
+int main(void)
+{int n;
+	n = 102;
+	printf('102%d\n', n);
+	reset_to_98(&n);
+	printf('102%\n', n);
+	return (0);
